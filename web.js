56de2c9f03cea5df088c692e4c5bde10fd960988
unknown <bboll@kent.edu>
2013-03-09 05:48:46 -0500

    web.js

--- web.js
+++ web.js
@@ -1,16 +1,5 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.set('view engine', 'html');
-
-app.engine('html', require('ejs').__express);
-
-app.get('/', function(request, response) {
-  response.render('index.html');
-});
+var connect = require('connect');
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+connect.createServer(connect.static(__dirname)).listen(port);
