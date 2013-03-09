e7aa35076255cd8a43fb4733a67150ac77cbe02b
unknown <bboll@kent.edu>
2013-03-09 03:52:14 -0500

    Serving HTML file

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('/index.html');
+  response.render('index.html');
 });
 
 var port = process.env.PORT || 5000;
