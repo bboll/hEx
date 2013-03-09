2984239a573b92eb6f50492185a407f283cfea24
unknown <bboll@kent.edu>
2013-03-09 05:56:50 -0500

    Fixed

--- web.js
+++ web.js
@@ -1,16 +1,24 @@
-var express = require('express');
+var http = require('http');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
-
-app.set('views', __dirname + '/views');
-
-app.engine('html', require('ejs').renderFile);
-
-app.get('/index', function(request, response) {
-  response.render('index.html');
-});
+http.createServer(function (request, response) {
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+    console.log('request starting...');
+    
+    fs.readFile('./index.html', function(error, content) {
+        if (error) {
+            response.writeHead(500);
+            response.end();
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+        }
+    });
+    
+}).listen(port);
+
+console.log('Listening on ' + port);
+
