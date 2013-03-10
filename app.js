afbf65eb078bb3b7c578eb79a3b891bb6be7d72b
unknown <bboll@kent.edu>
2013-03-10 08:06:02 -0400

    FQL attempts

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path')
-  , fql = require('fql');
+  , path = require('path');
 
 var app = express();
 
