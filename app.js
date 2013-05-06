f8af939140bff96b4faef380f0b02361d845de14
unknown <bboll@kent.edu>
2013-05-06 18:26:23 -0400

    Fix

--- app.js
+++ app.js
@@ -7,19 +7,28 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , pg = require('pg').native
+  , conString = process.env.DATABASE_URL || 'postgres://nytxdtfjjmtrww:JCV_IErkPLD8bzM1IvyzsYWFiA@ec2-54-235-155-40.compute-1.amazonaws.com:5432/d5k9e23rueegif'
+  , client;
 
-var pg = require('pg');
-var conString = process.env.DATABASE_URL || "postgres://nytxdtfjjmtrww:JCV_IErkPLD8bzM1IvyzsYWFiA@ec2-54-235-155-40.compute-1.amazonaws.com:5432/d5k9e23rueegif"
 
-var client = new pg.Client(conString);
+client = new pg.Client(conString);
+client.connect();
+
 app.get('/', function (req, res) {
-  pg.connect(conString, function (err, client, done) {
-    if(!err) return false;
-    done(client);
-    return true;
+  query = client.query('SELECT * FROM Person');
+  query.on('row', function(result) {
+    console.log(result);
+
+    if (!result) {
+      return res.send('No data found'); }
+    else {
+      res.send('Query complete');
+    }
   });
-})
+
+});
 
 /*
 var query = client.query("SELECT name FROM Person");
