ae9581ac04539451351090ff2c59d53ac46b6493
unknown <bboll@kent.edu>
2013-05-09 16:46:17 -0400

    JSON attempt

--- app.js
+++ app.js
@@ -38,18 +38,24 @@ http.createServer(app).listen(app.get('port'), function(){
 
 var outputFilename = './person.json';
 
+var myData = {
+  name: 'test',
+  version:'1.0'
+}
+
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   var query = client.query('SELECT * FROM Person');
 
   query.on('row', function(row) {
     //console.log(JSON.stringify(row));
-    fs.writeFile(outputFilename, JSON.stringify(row, null, 4), function (err){
+    fs.writeFile(outputFilename, JSON.stringify(myData, null, 4), function (err){
       if(err) {
         console.log(error);}
       else {
         console.log("JSON saved to person.json");
       }
     });
+
   });
 }); 
 
