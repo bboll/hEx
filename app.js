ab2cad77805c899f12d961ce261ea485bbe01ea8
unknown <bboll@kent.edu>
2013-05-09 17:16:44 -0400

    JSON attempt

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ var myData = {
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   var query = client.query('SELECT * FROM Person');
 
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-    //result.addRow(row);
+  query.on('row', function(row, result) {
+    //console.log(JSON.stringify(row));
+    result.addRow(row);
 
   });
 }); 
