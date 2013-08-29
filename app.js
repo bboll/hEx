d7a2855dae8da5a839c89b133c1d1b2d9412a471
unknown <bboll@kent.edu>
2013-08-29 18:27:26 -0400

    Attempt at fixing app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
 app.get('/users', user.list);
 
 function onRequest(request, response) {
