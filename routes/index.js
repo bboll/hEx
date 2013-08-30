a7fa2f6c6021a9f35772bd91b6d8f33c1c7f2cda
unknown <bboll@kent.edu>
2013-08-29 20:06:12 -0400

    Mistakes were made

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@
  */
 
 exports.index = function(req, res){
-  res.render('test.html', { title: 'Test' });
+  res.render('layout.jade', { title: 'Test' });
   console.log('Handling GET request');
 };
