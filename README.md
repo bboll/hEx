7169f3ab2bf4717424e0bec8a35bfac6e0408e6f
unknown <bboll@kent.edu>
2013-09-09 14:46:34 -0400

    Wiped node.js related files for PHP hail mary

--- README.md
+++
@@ -1,6 +0,0 @@
-hEx
-===
-
-A web application that integrates with Facebook and helps remove traces of a certain person.
-
-===
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('layout.jade', { title: 'Test' });
-  console.log('Handling GET request');
-};
